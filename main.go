eadba58058a2dda741b6d6d3c694d81b9605538f
Panos <panosynt@gmail.com>
2023-06-21 09:37:13 +0300

    create handlers and routes check post check

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", HomePage)
-	mux.HandleFunc("/bands", Bands)
-	mux.HandleFunc("/albums", Albums)
-	mux.HandleFunc("/songs", Songs)
+	mux.HandleFunc("/artist", Artist)
+	mux.HandleFunc("/album", Album)
+	mux.HandleFunc("/track", Track)
+
+	mux.HandleFunc("/artist/create", ArtistCreate)
+	mux.HandleFunc("/album/create", AlbumCreate)
+	mux.HandleFunc("/track/create", TrackCreate)
 
 	log.Println("Starting Server on :4000")
 	err := http.ListenAndServe(":4000", mux)
@@ -19,17 +23,51 @@ func main() {
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Homepage"))
 }
 
-func Bands(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Band"))
+func Artist(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("/artist"))
 }
 
-func Albums(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Albums"))
+func CheckPost(w http.ResponseWriter, r *http.Request) bool {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return false
+	}
+	return true
 }
 
-func Songs(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Songs"))
+func ArtistCreate(w http.ResponseWriter, r *http.Request) {
+	if !CheckPost(w, r) {
+		return
+	}
+	w.Write([]byte("/artist/create"))
+}
+
+func Album(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("/album"))
+}
+
+func AlbumCreate(w http.ResponseWriter, r *http.Request) {
+	if !CheckPost(w, r) {
+		return
+	}
+	w.Write([]byte("/album/create"))
+}
+
+func Track(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("/track"))
+}
+
+func TrackCreate(w http.ResponseWriter, r *http.Request) {
+	if !CheckPost(w, r) {
+		return
+	}
+	w.Write([]byte("/track/create"))
 }
