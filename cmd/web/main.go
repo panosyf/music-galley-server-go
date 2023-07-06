85f4d5adcc18016e422f84b800c3a226302fc4d6
Panos <panosynt@gmail.com>
2023-07-06 21:58:04 +0300

    routers.go file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 type application struct {
@@ -25,50 +24,13 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("./ui/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.homepage)
-	mux.HandleFunc("/artist", app.artist)
-	mux.HandleFunc("/artist/create", app.artistCreate)
-	mux.HandleFunc("/album", app.album)
-	mux.HandleFunc("/album/create", app.albumCreate)
-	mux.HandleFunc("/track", app.track)
-	mux.HandleFunc("/track/create", app.trackCreate)
-
 	srv := http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			if closeErr := f.Close(); closeErr != nil {
-				return nil, closeErr
-			}
-			return nil, err
-		}
-	}
-	return f, nil
 }
