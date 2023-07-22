226ec72425b81bc685940ebfb599e314477d7452
Panos <panosynt@gmail.com>
2023-07-22 22:29:45 +0300

    readable human dates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -25,6 +25,14 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	}
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -36,7 +44,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
